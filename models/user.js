f6a434572b8c306f6540b1402b922926706cfacb
Jvala2 <joevalancy@gmail.com>
2021-06-16 19:04:23 -0400

    user,js

--- models/user.js
+++ models/user.js
@@ -1,8 +1,13 @@
 const { Model, DataTypes } = require('sequelize');
-
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection.js');
 
-class User extends Model {}
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+      }
+
+}
 
 User.init(
   {
@@ -22,6 +27,12 @@ User.init(
     }
   },
   {
+    hooks: {
+        beforeCreate: async (newUserData) => {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
