d84673f92ac5c742dbe5b4b39e983c0fc664e606
psurer <surereker@icloud.com>
2021-06-21 20:52:25 +0200

    change routes, added test return data to auth routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,9 +5,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const eventsRoutes = require('./eventsRoutes');
 const planRoutes = require('./planRoutes');
+const authRoutes = require('./authRoutes');
 
 router.use('/events', eventsRoutes);
 router.use('/plans', planRoutes);
 router.use('/users', userRoutes);
+router.use('/auth', authRoutes);
 
 module.exports = router;
