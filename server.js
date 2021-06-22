c2dc3a56ebb77b0b49db1c3eb9b44caa5ab3dc98
psurer <surereker@icloud.com>
2021-06-23 01:47:39 +0200

    express redirect and html login button

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public','views')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(routes);
 
@@ -42,27 +42,4 @@ sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening ${PORT}`));
 }).catch(err => {
   console.log(err)
-});
-
-//Cognito pool configure
-auth.configure({
-    Auth: {
-        // REQUIRED - Amazon Cognito Region
-        Region: 'us-east-1',
-
-        // OPTIONAL - Amazon Cognito User Pool ID
-        UserPoolId: 'us-east-1_VYEOQTAu4', 
-
-        // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-        ClientId: '4vhr0obremuf9vd6jd7oksjb91',
-        
-        //  // OPTIONAL - Hosted UI configuration
-        // oauth: {
-        //     domain: 'likeminded.auth.us-east-1.amazoncognito.com',
-        //     scope: ['email', 'openid'],
-        //     redirectSignIn: 'http://localhost:3000/login',
-        //     redirectSignOut: 'http://localhost:3000/logout',
-        //     responseType: 'token' // or 'token', note that REFRESH token will only be generated when the responseType is code
-        // }
-    }
 });
